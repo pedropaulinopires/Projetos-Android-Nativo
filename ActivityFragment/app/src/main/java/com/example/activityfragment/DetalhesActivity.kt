fff1917d1e46b296c0b826f4bd121b2fe76d7ff4
Pedro Henrique <pedrohenrique92470@gmail.com>
2025-05-07 22:20:32 -0300

    Aprendendo estados de activities e bundle

--- ActivityFragment/app/src/main/java/com/example/activityfragment/DetalhesActivity.kt
+++ ActivityFragment/app/src/main/java/com/example/activityfragment/DetalhesActivity.kt
@@ -1,12 +1,16 @@
 package com.example.activityfragment
 
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class DetalhesActivity : AppCompatActivity() {
+
+    lateinit var buttonFechar: Button;
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -15,6 +19,16 @@ class DetalhesActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        buttonFechar = findViewById<Button>(R.id.button_fechar)
+
+        var bundle = intent.extras
+
+        buttonFechar.text = bundle?.getString("Texto") ?: "Não tem nada!"
+
+        buttonFechar.setOnClickListener {
+            finish()
         }
     }
 }
--- ActivityFragment/app/src/main/java/com/example/activityfragment/MainActivity.kt
+++ ActivityFragment/app/src/main/java/com/example/activityfragment/MainActivity.kt
@@ -1,14 +1,22 @@
 package com.example.activityfragment
 
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var buttonAbrir: Button;
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        Log.i("ciclo_vida", "onCreate")
+
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
@@ -16,5 +24,45 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        buttonAbrir = findViewById<Button>(R.id.button_abrir)
+        buttonAbrir.setOnClickListener {
+            var intent = Intent(this, DetalhesActivity::class.java)
+            intent.putExtra("Texto", "Voltar")
+
+            startActivity(intent)
+        }
     }
+
+    override fun onStart() {
+        super.onStart()
+        // carregar dados
+        Log.i("ciclo_vida", "onStart")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.i("ciclo_vida", "onResume")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.i("ciclo_vida", "onPause")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.i("ciclo_vida", "onStop")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Log.i("ciclo_vida", "onRestart")
+    }
+
+    override fun onDestroy() {
+        Log.i("ciclo_vida", "onDestroy")
+        super.onDestroy()
+    }
+
 }
