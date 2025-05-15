db13b5773dad1c341c0aa7bfd6aad5e77fcf7de4
Pedro Henrique <pedrohenrique92470@gmail.com>
2025-05-15 20:07:00 -0300

    Aprendendo sobre Data class e Destructuring

--- Colees/app/src/main/java/com/example/colees/ClasseAninhada.kt
+++ Colees/app/src/main/java/com/example/colees/ClasseAninhada.kt
@@ -5,16 +5,24 @@ class ClasseAninhada {
 }
 
 
-class Motorista(val nome: String){
-
-    inner class Caminhao(val modelo: String){
-        fun exibirDados() = println("Modelo: $modelo - Motorista $nome")
-    }
-}
+//class Motorista(val nome: String){
+//
+//    inner class Caminhao(val modelo: String){
+//        fun exibirDados() = println("Modelo: $modelo - Motorista $nome")
+//    }
+//}
 
 fun main() {
-    val motorista = Motorista("Pedro")
-    var caminhao = motorista.Caminhao("F250")
-    caminhao.exibirDados()
+//    val motorista = Motorista("Pedro")
+//    var caminhao = motorista.Caminhao("F250")
+//    caminhao.exibirDados()
+
+    var carro1 = Carro("Jetta", 2021)
+    var carro2 = Carro("Corolla", 2017)
+
+    var (modelo, ano) = carro1
+
 }
+
+data class Carro(val modelo: String, val ano: Int)
 
