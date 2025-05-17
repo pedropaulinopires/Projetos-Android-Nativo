096d7424c887aec5fd413f044f8a2dea852ad960
Pedro Henrique <pedrohenrique92470@gmail.com>
2025-05-17 10:42:43 -0300

    Apendendo sobre getter e setter, além do varags

--- Colees/app/src/main/java/com/example/colees/ClasseAninhada.kt
+++ Colees/app/src/main/java/com/example/colees/ClasseAninhada.kt
@@ -1,9 +1,29 @@
 package com.example.colees
 
+import android.R
+
 class ClasseAninhada {
 
 }
 
+class Pessoa(){
+    var nome: String = ""
+        get() = field.uppercase()
+    var idade: Int = 0
+        set(value) {
+            field = value
+        }
+    var telefones: List<String> = listOf("")
+
+    constructor(nome:String, idade: Int, vararg telefones: String) : this() {
+        this.nome = nome
+        this.idade = idade
+        this.telefones = telefones.toList()
+    }
+
+    var maiorIdade: Boolean = false
+        get() = idade >= 18
+}
 
 //class Motorista(val nome: String){
 //
@@ -17,10 +37,19 @@ fun main() {
 //    var caminhao = motorista.Caminhao("F250")
 //    caminhao.exibirDados()
 
-    var carro1 = Carro("Jetta", 2021)
-    var carro2 = Carro("Corolla", 2017)
+//    var carro1 = Carro("Jetta", 2021)
+//    var carro2 = Carro("Corolla", 2017)
+//
+//    var (modelo, ano) = carro1
 
-    var (modelo, ano) = carro1
+    var pessoa = Pessoa("Pedro", 15, "123", "1423", "78954")
+    print(pessoa.nome)
+    print(pessoa.idade)
+    print(pessoa.maiorIdade)
+    print(pessoa.telefones)
+
+
+
 
 }
 
