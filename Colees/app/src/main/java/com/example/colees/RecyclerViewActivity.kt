92135af418d42c559ba461173d50330d579a25b9
Pedro Henrique <pedrohenrique92470@gmail.com>
2025-05-26 06:17:34 -0300

    Aprendendo RecyclerView

---
+++ Colees/app/src/main/java/com/example/colees/RecyclerViewActivity.kt
@@ -0,0 +1,26 @@
+package com.example.colees
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.RecyclerView
+
+class RecyclerViewActivity : AppCompatActivity() {
+
+    lateinit var rvLista: RecyclerView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_recycler_view)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        rvLista = findViewById<RecyclerView>(R.id.rv_lista)
+    }
+}
