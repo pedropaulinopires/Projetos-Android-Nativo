9cabc2f86c523c9f3652bf0ecae5792f668830e8
Pedro Henrique <pedrohenrique92470@gmail.com>
2025-06-07 08:58:59 -0300

    Introdução ao view bind

---
+++ AndroidJetPack/app/src/main/java/com/fyra/androidjetpack/MainActivity.kt
@@ -0,0 +1,30 @@
+package com.fyra.androidjetpack
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var button: Button
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        button= findViewById(R.id.btn)
+        button.setOnClickListener {
+            Toast.makeText(this,"Teste", Toast.LENGTH_SHORT).show()
+        }
+    }
+}
