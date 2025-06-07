e86c0ccb09bfe989200de14654b7e98f974347cc
Pedro Henrique <pedrohenrique92470@gmail.com>
2025-06-06 23:52:23 -0300

    Criado projeto fyraFit

---
+++ FyraFit/app/src/main/java/com/fyra/fit/MainActivity.kt
@@ -0,0 +1,62 @@
+package com.fyra.fit
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import com.fyra.fit.ui.theme.FyraFitTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+
+        val splashScreen = installSplashScreen()
+
+        setContent {
+            FyraFitTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Column {
+        Text(text = "Olá, $name!")
+
+        Spacer(modifier = Modifier.height(16.dp)) // Espaçamento entre elementos
+
+        Button(onClick = { /* Ação do botão */ }) {
+            Text("Clique aqui")
+        }
+    }
+
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    FyraFitTheme {
+        Greeting("Android")
+    }
+}
---
+++ FyraFit/app/src/main/java/com/fyra/fit/ui/theme/Color.kt
@@ -0,0 +1,12 @@
+package com.fyra.fit.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val VermelhoForte = Color(0xFFF25252)
+val VermelhoFraco = Color(0xFFFF5757)
+val CinzaEscuro = Color(0xFF696565)
+val CinzaMedio = Color(0xFF9C9FA6)
+val CinzaClaro = Color(0xFFD7D9D8)
+val BrancoNeve = Color(0xFFF0F0F0)
+val Black = Color(0xFF000000)
+val White = Color(0xFFFFFFFF)
