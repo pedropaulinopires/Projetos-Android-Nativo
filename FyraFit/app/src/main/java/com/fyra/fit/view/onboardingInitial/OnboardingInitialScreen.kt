0e418035fc001647673a2c521d719fa247b5cea2
Pedro Henrique <pedrohenrique92470@gmail.com>
2025-06-14 13:15:21 -0300

    Update OnboardingInitialScreen.kt

--- FyraFit/app/src/main/java/com/fyra/fit/view/onboardingInitial/OnboardingInitialScreen.kt
+++ FyraFit/app/src/main/java/com/fyra/fit/view/onboardingInitial/OnboardingInitialScreen.kt
@@ -1,21 +1,8 @@
 package com.fyra.fit.view.onboardingInitial
 
-import androidx.compose.animation.AnimatedContent
-import androidx.compose.animation.AnimatedVisibility
-import androidx.compose.animation.animateContentSize
 import androidx.compose.animation.core.EaseInOut
-import androidx.compose.animation.core.FastOutSlowInEasing
-import androidx.compose.animation.core.animateDpAsState
-import androidx.compose.animation.core.animateFloatAsState
-import androidx.compose.animation.core.animateIntAsState
 import androidx.compose.animation.core.tween
-import androidx.compose.animation.fadeIn
-import androidx.compose.animation.fadeOut
-import androidx.compose.animation.slideInHorizontally
-import androidx.compose.animation.slideOutHorizontally
-import androidx.compose.animation.togetherWith
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.BoxWithConstraints
@@ -26,7 +13,6 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
@@ -39,7 +25,6 @@ import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
-import androidx.compose.material3.LinearProgressIndicator
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
@@ -52,13 +37,10 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.scale
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.graphics.vector.rememberVectorPainter
 import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.text.font.FontWeight
@@ -70,15 +52,12 @@ import com.fyra.fit.R
 import com.fyra.fit.model.onboardingInitial.OnboardingInitialItemModel
 import com.fyra.fit.ui.theme.CinzaEscuro
 import com.fyra.fit.ui.theme.CinzaMedio
-import com.fyra.fit.ui.theme.VermelhoFraco
 import com.fyra.fit.ui.theme.White
 import com.fyra.fit.ui.theme.fontapp_poppinsFamily
 import com.fyra.fit.ui.theme.responsiveSp
 import com.fyra.fit.viewmodel.onboardingInitial.OnboardingInitialViewModel
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.launch
-import org.koin.androidx.compose.koinViewModel
-import org.koin.compose.koinInject
 
 
 @OptIn(ExperimentalMaterial3Api::class)
